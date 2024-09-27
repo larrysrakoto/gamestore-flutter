90d5610b6b207775fd2de78c18f625fa5224cf47
larrysrakoto <larrysrakoto@gmail.com>
2024-09-27 18:20:28 +0300

    feat(home): creating homepgae

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,18 @@
+import 'package:finalgamestore/views/home/home.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main(){
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Home'),
-        ),
-      ),
+    return const MaterialApp(
+      title: 'Game',
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
   }
 }
-
---
+++ lib/views/details/details.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class DetailsPages extends StatelessWidget {
+  const DetailsPages({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
