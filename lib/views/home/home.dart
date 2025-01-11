b9d7f5b683a0544cd4b019a1139bd42b902df87f
larrysrakoto <larrysrakoto@gmail.com>
2025-01-11 14:34:38 +0300

    feat(widget): creating section

--- lib/views/home/home.dart
+++ lib/views/home/home.dart
@@ -1,3 +1,4 @@
+import 'package:finalgamestore/views/home/widgets/header.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,10 +6,57 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color(0xFF5F67EA),
+    return Scaffold(
+      backgroundColor: const Color(0xFF5F67EA),
       body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            Transform(
+            transform: Matrix4.identity()..rotateZ(20),
+              origin: const Offset((150), 50),
+               child: Image.asset('assets/images/bg_liquid.png', width: 200,)
+            ),
+            Positioned(
+              right: 0,
+              top: 200,
+              child: Transform(transform: Matrix4.identity()..rotateZ(20),
+              origin: const Offset(180, 100),
+                  child: Image.asset('assets/images/bg_liquid.png', width: 200,)
+            )
+            ),
+            Column(
+              children: [
+                HeaderSections(),
+              ],
+            )
+          ],
+        ),
       ),
+      bottomNavigationBar: NavigationBar(),
     );
   }
+}
+
+Widget NavigationBar () {
+  return BottomNavigationBar(
+    type: BottomNavigationBarType.fixed,
+    items: [
+      BottomNavigationBarItem(
+        label: 'Home',
+          icon: Icon(Icons.home),
+      ),
+      BottomNavigationBarItem(
+        label: 'Application',
+        icon: Icon(Icons.home),
+      ),
+      BottomNavigationBarItem(
+        label: 'Film',
+        icon: Icon(Icons.movie),
+      ),
+      BottomNavigationBarItem(
+        label: 'Book',
+        icon: Icon(Icons.book),
+      ),
+    ],
+  );
 }
---
+++ lib/views/home/widgets/header.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HeaderSections extends StatelessWidget {
+  const HeaderSections({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200,
+      color: Colors.red,
+    );
+  }
+}
